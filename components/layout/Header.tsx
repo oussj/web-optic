5b057ac62d38785bf1e9c4456f4783890776c91b
oussamah <oussamah.jaber@gmail.com>
2023-10-01 13:17:31 +0200

    chore: subheader

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,9 @@
-import { EnvelopeIcon, MapPinIcon, PhoneIcon } from "@heroicons/react/24/solid";
+import {
+  BuildingStorefrontIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+  PhoneIcon,
+} from "@heroicons/react/24/solid";
 import Image from "next/image";
 
 export const Header = () => {
@@ -14,25 +19,29 @@ export const Header = () => {
           />
         </div>
         <div className="flex flex-row">
-          <div className="flex items-center border border-zinc-700 px-1 lg:px-6">
+          <button className="bg-black hover:bg-blue-900 flex items-center border border-zinc-700 px-1 lg:px-6">
             <EnvelopeIcon className="h-8 w-8 text-white" />
-          </div>
-          <div className="flex items-center border border-zinc-700 px-1 lg:px-6">
+          </button>
+          <button className="bg-black hover:bg-blue-900 flex items-center border border-zinc-700 px-1 lg:px-6">
             <PhoneIcon className="h-8 w-8 text-white" />
-          </div>
-          <div className="flex items-center border border-zinc-700 px-1 lg:px-6">
+          </button>
+          <button className="bg-black hover:bg-blue-900 flex items-center border border-zinc-700 px-1 lg:px-6">
             <MapPinIcon className="h-8 w-8 text-white" />
-          </div>
+          </button>
         </div>
       </div>
       <div className="bg-white flex justify-between items-center h-[64px] shadow-lg px-16">
         <nav className="flex flex-row space-x-16">
-          <div>Lunettes de vue</div>
-          <div>Lunettes de soleil</div>
-          <div>Lentilles</div>
-          <div>Sport</div>
-          <div>News</div>
+          <div className="cursor-pointer hover:underline">Lunettes de vue</div>
+          <div className="cursor-pointer hover:underline">Lunettes de soleil</div>
+          <div className="cursor-pointer hover:underline">Lentilles</div>
+          <div className="cursor-pointer hover:underline">Sport</div>
+          <div className="cursor-pointer hover:underline">News</div>
         </nav>
+        <button className="bg-black hover:bg-blue-900 text-white px-4 py-2 flex flex-row justify-between items-center">
+          <BuildingStorefrontIcon className="h-8 w-8 text-white mr-2" />
+          Prendre RDV
+        </button>
       </div>
     </header>
   );
